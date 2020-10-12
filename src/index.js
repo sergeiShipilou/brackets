861600a5baa7397b4c1f54e615b1a608971949ab
Sergei Shipilov <sergeishipilou@yandex.by>
2020-10-12 21:42:06 +0300

    feat: add task solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let start = 1;
+
+  while (start === 1) {
+
+    let newStr = str;
+
+    bracketsConfig.map(item => str = str.replace(item[0] + item[1], ''));
+
+    if (newStr === str) {
+      start = 0;
+    }
+    
+  }
+  return (str.length === 0) ? true : false
 }
